8818790a7341e21e7005908700dd72781661fbc4
soufianeouz <ouzgani2006@gmail.com>
2026-09-04 20:16:00 +0100

    ...

--- src/__main__.py
+++ src/__main__.py
@@ -7,50 +7,57 @@ from llm_sdk import Small_LLM_Model
 from .models import OutputResult
 import os
 
+def main():
+    parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--input", default="data/input/tests.json")
-parser.add_argument("--output", default="data/output/function_calling_results.json")
-parser.add_argument(
-    "--functions_definition",
-    default="data/input/functions_definition.json",
-)
-
-args = parser.parse_args()
-module = Small_LLM_Model()
-
-data = read_json_files(args.functions_definition, args.input)
-
-
-final_result = []
-for prompt in data["prompts"]:
-    name_func = function_selector(
-        prompt["prompt"], data["functions"], module
+    parser.add_argument("--input", default="data/input/function_calling_tests.json")
+    parser.add_argument(
+        "--functions_definition",
+        default="data/input/functions_definition.json",
     )
-    for functions in data["functions"]:
-        if functions["name"] == name_func:
-            valid_function = functions
-            break
-    param = constrained_decoder(
-        prompt["prompt"], valid_function, module
-    )
+    parser.add_argument("--output", default="data/output/function_calling_results.json")
 
-    try:
-        OutputResult(
-            prompt=prompt["prompt"],
-            name=name_func,
-            parameters=param
+    args = parser.parse_args()
+    module = Small_LLM_Model()
+
+    data = read_json_files(args.functions_definition, args.input)
+
+
+    final_result = []
+    for i, prompt in enumerate(data["prompts"]):
+        print(f'[{i + 1}/{len(data["prompts"])}] {prompt["prompt"]}')
+        name_func = function_selector(
+            prompt["prompt"], data["functions"], module
         )
-        final_result.append({
-            "prompt": prompt["prompt"],
-            "name": name_func,
-            "parameters": param
-        })
-    except Exception as err:
-        print(f"Error: invalid output format → {err}")
-        exit(1)
-os.makedirs(os.path.dirname(args.output), exist_ok=True)
+        for functions in data["functions"]:
+            if functions["name"] == name_func:
+                valid_function = functions
+                break
+        param = constrained_decoder(
+            prompt["prompt"], valid_function, module
+        )
 
-with open(args.output, "w") as f:
-    json.dump(final_result, f, indent=4)
+        try:
+            OutputResult(
+                prompt=prompt["prompt"],
+                name=name_func,
+                parameters=param
+            )
+            final_result.append({
+                "prompt": prompt["prompt"],
+                "name": name_func,
+                "parameters": param
+            })
+        except Exception as err:
+            print(f"Error: invalid output format → {err}")
+            exit(1)
+    os.makedirs(os.path.dirname(args.output), exist_ok=True)
+
+    with open(args.output, "w") as f:
+        json.dump(final_result, f, indent=4)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt as e:
+        print(e)
